9a46805428144cfae9bc167fa4d12cfc572eef57
Az1z67 <abbasovaziz61@gmail.com>
2024-04-10 00:51:44 +0400

    order

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_inorder - goes through a binary tree using pre-order traversal
- * : pointer to the root node of the tree to traverse
+ * @tree pointer to the root node of the tree to traverse
  * @func:function to call in each node
  */
 
