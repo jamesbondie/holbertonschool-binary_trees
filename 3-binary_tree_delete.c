a1fe85ef205d2e95da7beb57b099d59a7979dc75
Az1z67 <abbasovaziz61@gmail.com>
2024-04-10 00:06:21 +0400

    commit

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_delete - free tree
+ * @tree: root node
+ */
+
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->right);
+
+	binary_tree_delete(tree->left);
+
+	free(tree);
+}
