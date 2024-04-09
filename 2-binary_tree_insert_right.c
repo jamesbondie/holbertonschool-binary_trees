139c8d6432350bca9f3f1301a15aa35db541c9c6
Az1z67 <abbasovaziz61@gmail.com>
2024-04-10 00:03:31 +0400

    commit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - insert in the left child of tree
+ * @parent: parent node
+ * @value: value
+ * Return: new node or NULL
+ */
+
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	new_node->right = parent->right;
+	if (new_node->right != NULL)
+	{
+		new_node->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
