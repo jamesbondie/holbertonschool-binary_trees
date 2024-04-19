ffc62ac12fd1d76c37b0ae470665f6400126c6c1
root <jamesalfreddie@gmail.com>
2024-04-19 19:57:40 +0000

    function works as a node measurer

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function that measures the nodes
+ * @tree:pointer to the root node
+ * Return: 0 or Size
+ */
+
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+int size = 0;
+if (tree == NULL)
+	return (0);
+if (tree->left == NULL || tree->right == NULL)
+	size = size + 1;
+size =  binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+return (size);
+}
