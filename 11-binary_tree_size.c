3908268dc6d29bc87a6f12883cce1357f38088be
root <jamesalfreddie@gmail.com>
2024-04-19 19:20:09 +0000

    betty fixed

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,9 +9,9 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int size = 0;
-	if (tree == NULL)
-		return (0);
-	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
-	return (size);
+int size = 0;
+if (tree == NULL)
+	return (0);
+size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+return (size);
 }
