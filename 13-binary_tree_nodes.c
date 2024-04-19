937a67abd33236e28f2852e0ccf4740d280c9ad2
root <jamesalfreddie@gmail.com>
2024-04-19 19:59:45 +0000

    function works as a node measurer

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -12,7 +12,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 int size = 0;
 if (tree == NULL)
 	return (0);
-if (tree->left == NULL || tree->right == NULL)
+if (tree->left != NULL || tree->right != NULL)
 	size = size + 1;
 size =  binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 return (size);
