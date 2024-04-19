da3524358b479b7e7116dc6c55f8515a7cace0e8
root <jamesalfreddie@gmail.com>
2024-04-19 20:44:41 +0000

    betty fixed and function works as node counter

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,9 +11,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 size_t size = 0;
 if (tree == NULL)
-	return (0);
-if (tree->left == NULL && tree->right == NULL)
-	size = size + 1;
-size =  binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-return (size);
+{
+if (tree->left != NULL || tree->right != NULL)
+nodes++;
+nodes += binary_tree_nodes(tree->left);
+nodes += binary_tree_nodes(tree->right);
+}
+return (nodes);
 }
