446e5bbd027eb7f50b95fee63ac111a37902a022
root <jamesalfreddie@gmail.com>
2024-04-21 09:48:50 +0000

    now this works as a function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,62 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - it measures height of the tree
- *
- * @tree: node to the root
- * Return: zero or condition
- *
+ * binary_tree_is_leaf - check if node id leaf or not
+ * @node: node
+ * Return: 1 or 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	else if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree:pointer to the root node
+ * Return: 0 or hight
  */
 
 
-
-int _binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-int left_height = _binary_tree_height(tree->left);
-int right_height = _binary_tree_height(tree->right);
-if (tree == NULL)
-	return (0);
-if (left_height > right_height)
-	return (left_height + 1);
-else
-	return (right_height + 1);
+	size_t h_left, h_right;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+	if (h_right <= h_left)
+		return (h_left + 1);
+	return (h_right + 1);
 }
 
 
+/**
+ * binary_tree_balance - balance of a binary tree
+ * @tree: pointer to root node
+ *
+ * Return: 0 or right
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
 
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return (left_height - right_height);
+}
 
 /**
  * binary_tree_is_perfect - it checks for if the tree is perfect
@@ -37,15 +70,17 @@ else
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-int height = _binary_tree_height(tree);
-int nodes = 1;
-int level = 0;
-if (tree == NULL)
+	int l, r, left, right;
+
+	if (tree == NULL)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	left = binary_tree_is_perfect(tree->left);
+	l = binary_tree_balance(tree->left);
+	right = binary_tree_is_perfect(tree->right);
+	r = binary_tree_balance(tree->right);
+	if (!(left - right)  && !(l - r))
+		return (1);
 	return (0);
-while (level < height - 1)
-{
-nodes *= 2;
-level++;
-}
-return (nodes == (1 << (height - 1)));
 }
